55fb1c296ef8a649df5889b0c238edcebbc765e5
MonicaArmanyos <36031900+MonicaArmanyos@users.noreply.github.com>
2018-04-27 01:50:18 +0200

    Update edit.blade.php

--- resources/views/managers/edit.blade.php
+++ resources/views/managers/edit.blade.php
@@ -22,7 +22,7 @@
                
                 <div class="form-group">
                   <label for="exampleInputNationalID">National ID</label>
-                  <input class="form-control" id="exampleInputNationalID"  type="number" name="national_id" value="{{$manager->national_id}}">
+                  <input class="form-control" id="exampleInputNationalID" pattern="[0-9]*" type="text" name="national_id" value="{{$manager->national_id}}">
                 </div>
                 <div class="form-group">
                 <label>Profile Image</label>
