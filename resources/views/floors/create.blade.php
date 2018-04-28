6c0ad8c08042a7caedad58562ec26c42d788bd10
ayaragab <aya.r.gad.94@gmail.com>
2018-04-28 18:23:15 +0200

    modify  Floor create.blade

--- resources/views/floors/create.blade.php
+++ resources/views/floors/create.blade.php
@@ -4,11 +4,24 @@
 
 <form method="post" action="/floors">
 {{csrf_field()}}
-Floor's  Name :- <input type="text" name="name">
 
+<div class="form-group row box-body">
+        
+        <label for="name" class="col-md-2 col-form-label text-md-right">Floor Name</label>
 
-<br>
-<input type="submit" value="Submit" class="btn btn-primary">
+        <div class="col-md-6">
+            <input id="name" type="text" class="form-control" name="name">
+        </div>
+    </div>
+
+<div class="form-group">
+        <div class="col-md-6 offset-md-4">
+            <button type="submit" class="btn btn-primary">
+                Create
+            </button>
+        </div>
+    </div>
+
 </form>
 
 @endsection
