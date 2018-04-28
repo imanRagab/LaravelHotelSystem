6b631d629021c824aef02e173a413046bded82fb
ayaragab <aya.r.gad.94@gmail.com>
2018-04-28 18:01:34 +0200

    update  Floor index.blade

--- resources/views/floors/index.blade.php
+++ resources/views/floors/index.blade.php
@@ -1,32 +1,74 @@
 @extends('layouts.admin.master')
 @section('content')
 
+<a href="/floors/create"><button class="btn btn-success">Create Floor</button></a>
+<br><br>
+<table class="table table-bordered" id="floors-table">
+        <thead>
+            <tr>
+                <th> Floor's Name</th>
+                <th>Floor's Number</th>
+                @role('admin')
+                <th>Created By</th>
+                @endrole
+                
+                <th>Action</th>
+                
+            </tr>
+        </thead>
+    </table>
 
-<body>
- <table border="1">
- <thead>
-<th> Floor's Name </th>
-<th> Floor's Number </th>
-<th>Manager's Name</th>
-<th> Actions </th>
-</thead>
-<tbody>
-@foreach ($floors as $floor)
-<tr> 
-    <td>{{$floor->name}}</td>
-    <td>{{$floor->floor_num}}</td>
-   <td>  hamada </td>
-   <!-- if current logged in is Admin -->
+    @stop
 
-    
-    <!-- if the manager created that floor -->
-    <td> <a href='floors/{{$floor->id}}/edit'> <input  value="edit" class="btn btn-primary"></a> </td>
-    <td> <a href='floors/{{$floor->id}}'>  <input  value="Delete" class="btn btn-primary"> </a> </td>
+    @push('js')
+    <script>
 
-</tr>
-@endforeach
-</tbody>
+    HTMLElement.prototype.delFloor = function(delUrl){
+        var resp = confirm("Are you sure you want to delete this room?");
+        if (resp == true) {
+            $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+            });
+            $.ajax(
+            {
+                url: delUrl,
+                type: 'delete',
+                dataType: "JSON",
+                data: "{}",
+                success: function (response)
+                {
+                    if(!response){
+                        alert("Floor rooms are not empty it can't be deleted!");
+                    }
+                    else{
+                        alert("Floor deleted successfully");
+                        window.location.href = "/floors";
+                    }  
+                    
+                }                
+            });
+        }
+    }    
+    $(function() {
+        $('#floors-table').DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: '{!! route('floors.data') !!}',
+            columns: [
+                { data: 'name', name: 'name' },
+                { data: 'number', name: 'number' },
+               
+                @role('admin')
+                {data: 'Manger_name', name: 'Manger_name'},
+                @endrole
+                {data: 'action', name: 'action'},
+              
 
-</table>
-</body>
-@endsection
+        ]
+            
+        });
+    });
+    </script>
+    @endpush
